d0218f5af5b7ccd51d06a6ff81e825c7bd31c047
Enguerran <engcolson@gmail.com>
2014-12-26 16:03:06 +0100

    use only npm

---
+++ js/app.js
@@ -0,0 +1,6 @@
+var React = require('react');
+
+React.render(
+    <h3>It works!</h3>,
+    document.getElementById('content')
+);
--- src/jsx/app.jsx
+++
@@ -1,6 +0,0 @@
-const React = require('react');
-
-React.renderComponent(
-    <h1>React boilerplate</h1>,
-    document.getElementById('content')
-);
